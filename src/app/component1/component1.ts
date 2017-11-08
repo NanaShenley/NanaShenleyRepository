3cfd95b60ce3d52844b77feccf6981cd01026676
nanashenley <nanashenley@yahoo.com>
2017-11-08 16:48:16 +0000

    Angular 4

---
+++ src/app/component1/component1.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'component1', 
+  templateUrl: './component1/component1.html',
+  styleUrls:  ['./component1/component1.css']
+})
+export class Component1 {
+       title = 'Component1';
+}
